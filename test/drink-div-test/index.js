6de423a3a955c46dce2a00777b563438fead6c2e
Rory Poulter <rorypoulter2@gmail.com>
2024-12-09 16:43:48 +0000

    Create function to create divs for all drinks of a given ingredient

--- test/drink-div-test/index.js
+++ test/drink-div-test/index.js
@@ -15,12 +15,11 @@ class DrinkDiv extends HTMLDivElement {
      * Adds the children nodes to the div
      * @param {*} param0 
      */
-    addChildren({name, url}){
-        console.log(name)
+    addChildren({strDrink, strDrinkThumb}){
         let name_header = document.createElement("h4");
-        name_header.innerHTML = name;
+        name_header.innerHTML = strDrink;
         let img = document.createElement("img");
-        img.setAttribute("src", url);
+        img.setAttribute("src", strDrinkThumb);
         this.appendChild(name_header);
         this.appendChild(img);
         this.appendChild(document.createElement("br"));
@@ -29,13 +28,34 @@ class DrinkDiv extends HTMLDivElement {
 };
 customElements.define("drink-div", DrinkDiv, {extends: "div"});
 
-let example_drinks = [
-    {name: "A Gilligan's Island", url: "https://www.thecocktaildb.com/images/media/drink/wysqut1461867176.jpg"},
-    {name: "A midsummernight dream", url: "https://www.thecocktaildb.com/images/media/drink/ysqvqp1461867292.jpg"}
-];
 
-for (let drink of example_drinks){
-    let drink_div = document.createElement("div", {is: "drink-div"});
-    drink_div.addChildren(drink);
-    document.getElementById("all_drinks").appendChild(drink_div.parent_div);
+async function createDivs(){
+    let response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Blue%20Curacao");
+    let json = await response.json();
+    let example_drinks = await json.drinks;
+
+    // let example_drinks = [
+    //     {name: "A Gilligan's Island", url: "https://www.thecocktaildb.com/images/media/drink/wysqut1461867176.jpg"},
+    //     {name: "A midsummernight dream", url: "https://www.thecocktaildb.com/images/media/drink/ysqvqp1461867292.jpg"}
+    // ];
+    let no_of_drinks = example_drinks.length;  // 94 drinks
+    let no_of_rows = Math.ceil(no_of_drinks / 4);
+    for (let i = 0; i < no_of_rows; i++){
+        let row_div = document.createElement("div");
+        row_div.setAttribute("class", "row");
+        row_div.setAttribute("id", "row"+i);
+        document.getElementById("all_drinks").appendChild(row_div);
+        for (let j = 0; j < 4; j++){
+            if (no_of_drinks - 1 < i * 4 + j){
+                break
+            }
+            let drink = example_drinks[i * 4 + j];
+            let drink_div = document.createElement("div", {is: "drink-div"});
+            drink_div.addChildren(drink);
+            document.getElementById("row"+i).appendChild(drink_div.parent_div);
+        };
+    document.getElementById("all_drinks").appendChild(document.createElement("br"))
+    }
 };
+
+createDivs();
